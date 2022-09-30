5c0d8cee67fd5cfa2b99a0dc3b1bcd70e3cff7b0
DevRequena <devcesarperez@gmail.com>
2022-09-30 12:44:59 -0400

    Numeral

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function CTA() {
   return (
     <div className='cta'> 
-    <a href={"#"} rel="noreferrer" download className='btn'>Download CV</a>
+    <a href={"#download"} rel="noreferrer" download className='btn'>Download CV</a>
     <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
